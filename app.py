3cc9e0df4aa6d6752453fed83155308579229e32
Isac Moura <contatoisacmoura@outlook.com>
2019-01-23 01:54:01 -0300

    Código demo Jinja 2

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('home.jinja2')
 
-
+@app.route('/demo')
+def demo():
+    return render_template('demo.jinja2')
+    
 app.run(debug=True)
